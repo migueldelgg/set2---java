11f90f1ae54257b9e74983dc960641e93ccc259b
4Miguel2 <delgdevoleper@gmail.com>
2024-01-31 13:59:37 -0300

    How many students for course n ?

---
+++ src/entities/User.java
@@ -0,0 +1,33 @@
+package entities;
+
+import java.util.Objects;
+
+public class User {
+
+    private Integer userId;
+
+    public User(Integer userId) {
+        this.userId = userId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId);
+    }
+}
